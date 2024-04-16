a5aa39e40d38bd3c6183c6e8dd63105369490987
alexey.petrinich <alexey.petrinich@gmail.com>
2024-04-16 10:22:12 +0200

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Aliaksei Piatrynich on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
